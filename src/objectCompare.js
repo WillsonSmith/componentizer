5451106765b1af0b5a47179b25c7726b741c19e5
Willson Smith <me@willsonsmith.com>
2016-12-13 00:00:50 -0500

    remove old file

--- src/objectCompare.js
+++
@@ -1,5 +0,0 @@
-export default function compare(objectOne, objectTwo) {
-  const objectOneKeys = Object.keys(objectOne);
-  const objectTwoKeys = Object.keys(objectTwo);
-  const diffs = objectOneKeys.filter((key) => objectTwoKeys.indexOf(key) === -1);
-}
